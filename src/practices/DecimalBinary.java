9c9d0c1cb3178e3e2fc736594ca5f41e133d545d
sgs09 <sgs09@ALBERT-G15>
2021-11-25 02:14:33 -0500

    Changed layout.

--- src/practices/DecimalBinary.java
+++ src/practices/DecimalBinary.java
@@ -29,35 +29,25 @@ public class DecimalBinary extends Application {
 	@Override
 	public void start(Stage pPrimaryStage) throws Exception {
 		Model model = new Model();
-    	
-    	GridPane root = createPane(); // The root of the GUI component graph
 
-    	Text decimalText = new Text("Decimal");
-    	decimalText.setTextAlignment(TextAlignment.CENTER);
+		GridPane root = createPane(); // The root of the GUI component graph
 
-    	Text binaryText = new Text("Binary");
-    	binaryText.setTextAlignment(TextAlignment.CENTER);
-    	
-    	root.add(decimalText, 0, 0, 1, 1);
-    	root.add(binaryText, 0, 1, 1, 1);
-    	root.add(new DecimalObserver(model), 1, 0, 1, 1);
-    	root.add(new BinaryObserver(model), 1, 1, 1, 1);
-    	
-    	
-    	pPrimaryStage.setTitle("Decimal <-> Binary");
-    	pPrimaryStage.setResizable(false);
-        pPrimaryStage.setScene(new Scene(root));
-        pPrimaryStage.show();
+		root.add(new DecimalObserver(model), 0, 0, 1, 1);
+		root.add(new BinaryObserver(model), 0, 1, 1, 1);
+
+		pPrimaryStage.setTitle("Decimal <-> Binary");
+		pPrimaryStage.setResizable(false);
+		pPrimaryStage.setScene(new Scene(root));
+		pPrimaryStage.show();
 	}
-	
-	private static GridPane createPane()
-    {
-    	GridPane root = new GridPane();
-    	root.setHgap(GAP);
-    	root.setVgap(GAP);
-    	root.setPadding(new Insets(MARGIN));
-    	return root;
-    }
+
+	private static GridPane createPane() {
+		GridPane root = new GridPane();
+		root.setHgap(GAP);
+		root.setVgap(GAP);
+		root.setPadding(new Insets(MARGIN));
+		return root;
+	}
 
 }
 
@@ -90,29 +80,32 @@ class Model {
 	}
 
 	private void notifyObservers() {
-		for(Observer obs : this.aObservers) {
+		for (Observer obs : this.aObservers) {
 			obs.numberChanged(getNumber());
 		}
 	}
 }
 
 class DecimalObserver extends Parent implements Observer {
-	private TextField aText = new TextField();
+	private TextField aTextField = new TextField();
+	private Text aText = new Text("Decimal");
 	private Model aModel;
 
 	DecimalObserver(Model pModel) {
 		this.aModel = pModel;
 		aModel.addObserver(this);
-		aText.setMinWidth(DecimalBinary.WIDTH);
-		aText.setPromptText("Enter a Decimal");
+		aTextField.setMinWidth(DecimalBinary.WIDTH);
+		aTextField.setPromptText("Enter a Decimal");
+		aText.setTextAlignment(TextAlignment.CENTER);
+		getChildren().add(aTextField);
 		getChildren().add(aText);
 
-		aText.setOnAction(new EventHandler<ActionEvent>() {
+		aTextField.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent pEvent) {
 				int lInteger = 0;
 				try {
-					lInteger = Integer.parseInt(aText.getText());
+					lInteger = Integer.parseInt(aTextField.getText());
 				} catch (NumberFormatException pException) {
 					// Just ignore. We'll use 0 instead.
 				}
@@ -123,30 +116,33 @@ class DecimalObserver extends Parent implements Observer {
 
 	@Override
 	public void numberChanged(int pNumber) {
-		aText.setText(Integer.valueOf(pNumber).toString());
+		aTextField.setText(Integer.valueOf(pNumber).toString());
 	}
 
 }
 
 class BinaryObserver extends Parent implements Observer {
-	
-	private TextField aText = new TextField();
+
+	private TextField aTextField = new TextField();
+	private Text aText = new Text("Binary");
 	private Model aModel;
 
 	BinaryObserver(Model pModel) {
 		this.aModel = pModel;
 		aModel.addObserver(this);
-		aText.setMinWidth(DecimalBinary.WIDTH);
-		aText.setPromptText("Enter a Binary");
+		aTextField.setMinWidth(DecimalBinary.WIDTH);
+		aTextField.setPromptText("Enter a Binary");
+		aText.setTextAlignment(TextAlignment.CENTER);
+		getChildren().add(aTextField);
 		getChildren().add(aText);
 
-		aText.setOnAction(new EventHandler<ActionEvent>() {
+		aTextField.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent pEvent) {
 				int lInteger = 0;
 				try {
-					lInteger = Integer.parseInt(aText.getText());
-					if(testBinary(lInteger) == false) {
+					lInteger = Integer.parseInt(aTextField.getText());
+					if (testBinary(lInteger) == false) {
 						lInteger = 0;
 					}
 				} catch (NumberFormatException pException) {
@@ -159,32 +155,32 @@ class BinaryObserver extends Parent implements Observer {
 
 	@Override
 	public void numberChanged(int pNumber) {
-		aText.setText(Integer.toBinaryString(pNumber));
+		aTextField.setText(Integer.toBinaryString(pNumber));
 	}
-	
+
 	private static boolean testBinary(int pNumber) {
-		String[] TEST_SUBJECT = {"2", "3", "4", "5", "6", "7", "8", "9"};
-		
+		String[] TEST_SUBJECT = { "2", "3", "4", "5", "6", "7", "8", "9" };
+
 		String s = Integer.toString(pNumber);
-		
-		for(int i = 0; i < TEST_SUBJECT.length; i++) {
-			if(s.contains(TEST_SUBJECT[i])) {
+
+		for (int i = 0; i < TEST_SUBJECT.length; i++) {
+			if (s.contains(TEST_SUBJECT[i])) {
 				return false;
 			}
 		}
-		
+
 		return true;
-		
+
 	}
-	
+
 	private static int binaryToDecimal(int pBinary) {
 		String s = Integer.toString(pBinary);
 		int result = 0;
-		
-		for(int i = 0; i < s.length(); i++) {
+
+		for (int i = 0; i < s.length(); i++) {
 			result += Math.pow(2, i) * (s.charAt(s.length() - i - 1) - '0');
 		}
-		
+
 		return result;
 	}
 
